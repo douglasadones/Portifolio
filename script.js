8b15d986d1b1dab040a3891c632b89140ebeaeb8
Douglas A.S.N <contato.douglas.ad@gmail.com>
2024-05-07 08:02:40 -0300

    adicionada logica row column

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 let allProjectButtons = document.querySelectorAll("div.project");
 
-console.log(allProjectButtons);
+const thereAreDisplayBlockChecker = () => {
+    for (const i of allProjectButtons) {
+        if (i.lastElementChild.style.display === "block") {
+            console.log("Achei!")
+            return true;
+        }
+    }
+    return false;
+};
+
+
 
 window.addEventListener("DOMContentLoaded", () => {
 
     allProjectButtons.forEach(element => {
-        console.log(element.children);
         element.addEventListener("click", () => {
-            
             let projectContent = element.lastElementChild;
-
             if (projectContent.style.display === "block") {
-
-
-                // document.querySelector(".row").style.flexDirection = "column";
 
                 projectContent.style.display = "none";
 
+                // Não havendo nenhuma imagem expandida, volta ao display row.
+                if (!thereAreDisplayBlockChecker()) {
+                    document.querySelector(".row").style.flexDirection = "row";
+                }              
+
             } else {
 
-                // document.querySelector(".row").style.flexDirection = "row";
+                document.querySelector(".row").style.flexDirection = "column";
 
                 projectContent.style.display = "block";
-
-
             }
         })
     });
