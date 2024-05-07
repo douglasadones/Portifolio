7779abf9f9501584b1c7c77b52df5f3dfd7f928a
Douglas A.S.N <contato.douglas.ad@gmail.com>
2024-05-06 21:41:34 -0300

    adicionando novas ideias

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-const flutter_p1_gif = 'https://user-images.githubusercontent.com/95550011/194463089-fa489bb5-50ab-4350-923d-75c903e7d880.gif';
-
-
 let allProjectButtons = document.querySelectorAll("div.project");
 
 console.log(allProjectButtons);
 
 window.addEventListener("DOMContentLoaded", () => {
+
     allProjectButtons.forEach(element => {
         console.log(element.children);
         element.addEventListener("click", () => {
             
             let projectContent = element.lastElementChild;
 
-            if (projectContent.style.display === "none") {
+            if (projectContent.style.display === "block") {
 
 
-                document.querySelector(".row").style.flexDirection = "column";
-
-                projectContent.style.display = "block";
-
-            } else {
-
-                document.querySelector(".row").style.flexDirection = "row";
+                // document.querySelector(".row").style.flexDirection = "column";
 
                 projectContent.style.display = "none";
 
-                
+            } else {
+
+                // document.querySelector(".row").style.flexDirection = "row";
+
+                projectContent.style.display = "block";
+
 
             }
         })
     });
+
 });
